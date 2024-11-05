9fbc7ec4b6bd050f7b55d882b22a360c8529cd2b
TungDucVu <147933411+TungDucVu@users.noreply.github.com>
2024-11-05 21:48:58 +0530

    Update fib.c

--- fib.c
+++ fib.c
@@ -1,12 +1,18 @@
 /*
  * No changes are allowed in this file
  */
+int arr[1024];
+
 int fib(int n) {
   if(n<2) return n;
   else return fib(n-1)+fib(n-2);
 }
 
 int _start() {
+   for (int i = 0; i < 1024; i++) {
+      arr[i] = fib(3);
+
+   }
 	int val = fib(40);
 	return val;
 }
